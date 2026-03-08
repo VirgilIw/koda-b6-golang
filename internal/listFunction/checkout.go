d1cab5ebf900f3e4cb08b2ca417dbd75f2c1d415
VirgilIw <imanuelambar11@gmail.com>
2026-03-08 19:27:17 +0700

    feat: add mutex

--- internal/listFunction/checkout.go
+++ internal/listFunction/checkout.go
@@ -2,13 +2,18 @@ package listfunction
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/virgilIw/koda-b6-golang/model"
 )
 
 var histori []model.ItemKeranjang
+var mu sync.Mutex
 
 func Checkout(keranjang *[]model.ItemKeranjang) {
+
+	mu.Lock()
+	defer mu.Unlock()
 
 	if len(*keranjang) == 0 {
 		fmt.Println("Keranjang kosong")
